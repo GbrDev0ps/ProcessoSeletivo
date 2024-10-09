de0d991a526999b38f8be54e44a85e76cedaa7cf
GbrDev0ps <gabriel.monte.aluno.institute@gmail.com>
2024-10-08 22:28:32 -0300

    Making the logic better

--- ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -10,11 +10,11 @@ public class ProcessoSeletivo {
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
-            System.out.println("LIGAR PARA O CANDIDATO");
-        } else if(salarioBase == salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
-        } else {
+        } else if(salarioBase == salarioPretendido) {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        } else {
+            System.out.println("LIGAR PARA O CANDIDATO");
         }
                      
     }
